48c6458572ea9b667fabc9d30583818af89efd64
Arjoo <2009arjoo@gmail.com>
2021-04-06 18:19:00 +0530

    Initial commit

---
+++ src/main/java/com/arjoo/spring/cloud/config/storage/SpringCloudConfigStorageApplication.java
@@ -0,0 +1,13 @@
+package com.arjoo.spring.cloud.config.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigStorageApplication.class, args);
+	}
+
+}
